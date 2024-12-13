32848fc2e7605413c8541e8b4784b37414c6cc29
Felipe Santos <felipecassiors@gmail.com>
2024-12-13 10:44:46 -0300

    Remove unnecessary check for spaces in PATH

--- support/dependencies/dependencies.sh
+++ support/dependencies/dependencies.sh
@@ -38,9 +38,9 @@ case ":${PATH:-unset}:" in
 	echo "PATH environment variable. This doesn't work."
 	exit 1
 	;;
-(*" "*|*"${TAB}"*|*"${NL}"*)
+(*"${TAB}"*|*"${NL}"*)
 	printf "\n"
-	printf "Your PATH contains spaces, TABs, and/or newline (\\\n) characters.\n"
+	printf "Your PATH contains TABs and/or newline (\\\n) characters.\n"
 	printf "This doesn't work. Fix you PATH.\n"
 	exit 1
 	;;
